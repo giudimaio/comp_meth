caa40d3dadf5975c1fed622962d7bb0714ac73ff
giudimaio <dimaio.giuseppe@outlook.com>
2020-10-06 17:33:20 +0200

    Modified comments added

--- 1_assignment/assegnamento1.py
+++ 1_assignment/assegnamento1.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """First assignment.
 """
 
@@ -9,38 +8,67 @@ import string
 
 logging.basicConfig(level=logging.DEBUG)
 
-
+#Define a function which needs the file path (or simply file name if it is in the same dir):
 def process(file_path):
-    """Reads a text file and compile the letter statistics."""
-    start_time = time.time()
 
+    start_time = time.time()
+    
+	#Display it's correctly reading the file:
     logging.info("Reading input file %s...", file_path)
-    with open(file_path) as input_file:
+    #The following opens the file by taking the file_path:
+    with open(file_path) as input_file:					
         text = input_file.read()
+    
     num_chars = len(text)
     logging.info("Done, %d characters found.", num_chars)
 
-    # char_dict = {chr(x): 0 for x in range(ord('a'), ord('z') + 1)}
+	#Create a dict.
+    #The syntax for dict is {key:value} with key variable in this case (ch represents the character in ascii string). 
+    #string.ascii gives a string 'abcdefgh...', while '0' stands for the value I want to associate to the keys 'a', 'b',
+    #.....
     char_dict = {ch: 0 for ch in string.ascii_lowercase}
+
+	#Takes the ch-key of the input text:
     for ch in text:
+    	#.lower() converts all uppercases to lowercases, for example 'A' ---> 'a':
         ch = ch.lower()
+        #Implements the counter for obtaining frequencies. If the ch-key appears in dict --> 
+        # ---> increments the VALUE related to CH-KEY:
         if ch in char_dict:
             char_dict[ch] += 1
+            
+        #Another way to do it (not so obvious..):
         # try:
         #     char_dict[ch.lower()] += 1
         # except KeyError:
         #     pass
 
+	#The following gives the elapsed time from start (start_time) to now (time.time):
     elapsed_time = time.time() - start_time
     logging.info("Done in %.3f seconds.", elapsed_time)
+    
+    #Sum the letters (VALUES IN DICT) to find the total number of letters (not characters!):
     num_letters = sum(char_dict.values())
+    #Go inside the dict {ch:num} and divide the num (frequency for 'a', for example) by total number, for
+    # having the percent:
     for ch, num in char_dict.items():
+    	#.....I dont understand the purpose of f inside print.....
         print(f"{ch} -> {num / num_letters:.3%}")
 
 
+#"if" is for doing everything in the __main__:
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser()
     parser.add_argument('infile', type=str, help="Path to the input file")
     args = parser.parse_args()
 
     process(args.infile)
+    #print(args.infile)
+    
+    
+    
+    
+    
+    
+    
